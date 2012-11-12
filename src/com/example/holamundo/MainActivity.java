91f0eeb70f205d86515c7d565a0b37928e1cf04f
tutor-pg <usuario@servidor>
2012-11-12 18:00:19 +0100

    Aplicación PhoneGap

---
+++ src/com/example/holamundo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.holamundo;
+
+import android.os.Bundle;
+import android.view.Menu;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
